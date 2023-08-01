e1bca511834fa74b6c5cf53bf2adb619e1b724cf
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-08-01 12:07:32 +0800

    feat: create 2 sections in summary container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import "./App.scss";
 function App() {
   return (
     <div className="app hvc">
-      <div className="summary-ctn"></div>
+      <div className="summary-ctn">
+        <div className="result-display"></div>
+        <div className="result-summary"></div>
+      </div>
     </div>
   );
 }
