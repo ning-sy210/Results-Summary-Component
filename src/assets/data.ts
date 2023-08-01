cf1a2b7741a50f5f23178d9624c2a7293029bc4f
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-08-01 15:50:18 +0800

    chore: switch to TS with image imports

---
+++ src/assets/data.ts
@@ -0,0 +1,29 @@
+import IconReaction from "./images/icon-reaction.svg";
+import IconMemory from "./images/icon-memory.svg";
+import IconVerbal from "./images/icon-verbal.svg";
+import IconVisual from "./images/icon-visual.svg";
+
+const ScoreCards = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: IconReaction,
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: IconMemory,
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: IconVerbal,
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: IconVisual,
+  },
+];
+
+export default ScoreCards;
