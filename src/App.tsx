ac7fbc1617f1308a0a6b34a8590c03b14483d504
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-08-01 13:09:43 +0800

    feat: add elements into result display with minimal css

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import "./App.scss";
 
 function App() {
   return (
-    <div className="app hvc">
+    <div className="app stack-hvc">
       <div className="summary-ctn">
-        <div className="result-display"></div>
+        <div className="result-display stack-hc">
+          <span>Your Result</span>
+
+          <div className="score-display stack-hvc">
+            <span>76</span>
+            <span>of 100</span>
+          </div>
+
+          <div>
+            <span>Great</span>
+            <p>
+              You scored higher than 65% of the people who have taken the tests.
+            </p>
+          </div>
+        </div>
         <div className="result-summary"></div>
       </div>
     </div>
