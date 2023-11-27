45da8cddce453cb87cce784a588abcfadb7b043d
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-27 21:33:35 +0800

    refactor: remove redundant classes

--- src/assets/components/ScoreBreakdown/ScoreDisplay/ScoreDisplay.tsx
+++ src/assets/components/ScoreBreakdown/ScoreDisplay/ScoreDisplay.tsx
@@ -21,10 +21,10 @@ const ScoreDisplay = ({ categoryLabel, score, icon }: ScoreDisplayProps) => {
         <span className="category-max-score">/ 100</span>
       </div>
 
-      <div className={`top left corner corner--top-left`}></div>
-      <div className={`top right corner corner--top-right`}></div>
-      <div className={`bottom left corner corner--bottom-left`}></div>
-      <div className={`bottom right corner corner--bottom-right`}></div>
+      <div className={`corner top-left`}></div>
+      <div className={`corner top-right`}></div>
+      <div className={`corner bottom-left`}></div>
+      <div className={`corner bottom-right`}></div>
     </div>
   );
 };
